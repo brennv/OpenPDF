01ffcd94d966f6bdd144e31113cf4db375cd4772
brennv <brennv@users.noreply.github.com>
2016-06-08 21:37:17 -0700

    remove oops

--- pdf-json/src/main/java/com/lowagie/json/PdfParser.java
+++ pdf-json/src/main/java/com/lowagie/json/PdfParser.java
@@ -5,7 +5,6 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
-import com.lowagie.text
 import com.lowagie.text.Document;
 import com.lowagie.text.DocumentException;
 import com.lowagie.text.pdf.PdfCopy;
